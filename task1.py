1dd9865677245bf1fe0dc27a41a4c17413a194e7
rutujjjja <rutujapatil092006@gmail.com>
2025-07-24 21:30:13 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,19 @@
+# The first task
+num1= float(input("Enter number 1:"))
+num2=float(input("Enter number 2:"))
+
+#addition
+sum= num1 +num2
+print("Addition:",sum)
+
+#substraction
+sub= num1 - num2
+print("Substraction:",sub)
+
+#multiplication
+mul= num1 *num2
+print("Multiplication:",mul)
+
+#Division
+div= num1 / num2
+print("Division:",div)
